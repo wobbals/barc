09e008fa38ba5ec397201acdcade723eb9a833b8
Charley Robinson <charley@tokbox.com>
2017-02-09 14:48:14 -0700

    CHECKPOINT Unzip a zip or work from specified directory

--- barc/main.c
+++ barc/main.c
@@ -17,6 +17,12 @@
 #include <libswresample/swresample.h>
 #include <assert.h>
 #include <MagickWand/MagickWand.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ftw.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <zip.h>
 
 #include "yuv_rgb.h"
 #include "archive_stream.h"
@@ -24,8 +30,8 @@
 #include "magic_frame.h"
 #include "audio_mixer.h"
 
-const int out_width = 640;
-const int out_height = 480;
+const int out_width = 1280;
+const int out_height = 720;
 const int out_pix_format = AV_PIX_FMT_YUV420P;
 const int out_audio_format = AV_SAMPLE_FMT_FLTP;
 const int src_audio_format = AV_SAMPLE_FMT_S16;
@@ -677,6 +683,93 @@ void my_log_callback(void *ptr, int level, const char *fmt, va_list vargs)
     vprintf(fmt, vargs);
 }
 
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf)
+{
+    int rv = remove(fpath);
+
+    if (rv)
+        perror(fpath);
+
+    return rv;
+}
+
+int rmrf(const char *path)
+{
+    return nftw(path, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+}
+
+static void safe_create_dir(const char *dir)
+{
+    if (mkdir(dir, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) < 0) {
+        if (errno != EEXIST) {
+            perror(dir);
+            exit(1);
+        }
+    }
+}
+
+char* unzip_archive(const char* path) {
+    int err, i, fd;
+    int64_t sum, len;
+    struct zip_file *zf;
+    struct zip_stat sb;
+    char buf[100];
+    struct zip* za = zip_open(path, ZIP_CHECKCONS | ZIP_RDONLY, &err);
+    if (NULL == za) {
+        zip_error_to_str(buf, sizeof(buf), err, errno);
+        printf("can't open zip archive `%s': %s/n",
+                path, buf);
+        return NULL;
+    }
+
+    const char* working_directory = "out";
+    rmrf(working_directory);
+    safe_create_dir(working_directory);
+    err = chdir(working_directory);
+
+    for (i = 0; i < zip_get_num_entries(za, 0); i++) {
+        if (zip_stat_index(za, i, 0, &sb) == 0) {
+            printf("==================\n");
+            len = strlen(sb.name);
+            printf("Name: [%s], ", sb.name);
+            printf("Size: [%llu], ", sb.size);
+            printf("mtime: [%u]\n", (unsigned int)sb.mtime);
+            if (sb.name[len - 1] == '/') {
+                safe_create_dir(sb.name);
+            } else {
+                zf = zip_fopen_index(za, i, 0);
+                if (!zf) {
+                    fprintf(stderr, "boese, boese\n");
+                    exit(100);
+                }
+
+                fd = open(sb.name, O_RDWR | O_TRUNC | O_CREAT, 0644);
+                if (fd < 0) {
+                    fprintf(stderr, "boese, boese\n");
+                    exit(101);
+                }
+
+                sum = 0;
+                while (sum != sb.size) {
+                    len = zip_fread(zf, buf, 100);
+                    if (len < 0) {
+                        fprintf(stderr, "boese, boese\n");
+                        exit(102);
+                    }
+                    write(fd, buf, len);
+                    sum += len;
+                }
+                close(fd);
+                zip_fclose(zf);
+            }
+        } else {
+            printf("File[%s] Line[%d]/n", __FILE__, __LINE__);
+        }
+    }
+    char* pwd = getcwd(NULL, 0);
+    return pwd;
+}
+
 int main(int argc, char **argv)
 {
     //av_log_set_level(AV_LOG_VERBOSE);
@@ -689,8 +782,33 @@ int main(int argc, char **argv)
 
     MagickWandGenesis();
 
+    char* path;
+    if (1 >= argc) {
+        path = "/Users/charley/src/barc/sample/allhands_sample.zip";
+    } else {
+        path = argv[1];
+    }
+
+    struct stat file_stat;
+    stat(path, &file_stat);
+
+    if (S_ISDIR(file_stat.st_mode)) {
+        printf("using directory %s\n", path);
+    } else if (S_ISREG(file_stat.st_mode)) {
+        printf("attempt to unzip regular file %s\n", path);
+        path = unzip_archive(path);
+    } else {
+        printf("Unknown file type %s\n", path);
+        exit(-1);
+    }
+
+    if (!path) {
+        printf("No working path. Exit.\n");
+        exit(-1);
+    }
+
     struct archive_t* archive;
-    archive_open(&archive, out_width, out_height, "/Users/charley/src/barc/sample/product_meeting_sample");
+    archive_open(&archive, out_width, out_height, path);
 
     open_output_file("output.mp4");
     
